aa28a460df310e947064953ce359e485c29487ae
koyamal <tanakamaruko1121n@gmail.com>
2024-04-21 17:06:51 +0900

    UserInfoコンポーネントを作成

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,9 @@
 import React, { useState, useEffect} from "react";
-
-type User = {
-  age: number;
-  name: string;
-};
+import UserInfo from "./screens/UserInfo";
 
 const App: React.FC = () => {
-  const [userInfo, setUserInfo] = React.useState<User[] | null>(null);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch("/firestore/get");
-        const json: React.SetStateAction<User[] | null> = await res.json();
-        setUserInfo(json);
-        console.log(json);
-      } catch (e) {
-        console.log(e);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
-    <div className="container">
-      <h1>User Info</h1>
-      {userInfo?.map((user) => (
-          <div key={user.name}>
-            <h3>{user.name}</h3>
-            <p>{user.age}</p>
-          </div>
-        )
-      )}
-    </div>
+    <UserInfo />
   );
 };
 
