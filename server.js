8b218e6602bbfb966b4bf368438ff5abefd13852
koyamal <tanakamaruko1121n@gmail.com>
2024-04-21 11:09:12 +0900

    Firestoreに接続できるよう修正した

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 import express from 'express';
+import { Firestore } from '@google-cloud/firestore';
+import dotenv from 'dotenv';
+
 const app = express();
+
+dotenv.config();
+const projectId = process.env.GCP_PROJECT;
+const gcpOptions = {
+  projectId,
+  ...(process.argv[2] === 'local' && { keyFilename: process.env.FIRESTORE_PRIVATE_KEY_FILE}),
+};
+
+const firestore = new Firestore(gcpOptions);
 
 app.get('/', (req, res) => {
   res.send('Hello from App Engine!');
+});
+
+app.get('/firestore/get', async (req, res) => {
+  const ref = await firestore.collection("test").get();
+  console.log(ref);
+  const output = [];
+  for (const doc of ref.docs) {
+    const data = doc.data();
+    output.push(data);
+  }
+  res.send(output);
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
