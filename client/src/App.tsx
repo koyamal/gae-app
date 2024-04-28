3f3312f3d6c8c7a2dfe05602babd910222de79f8
koyamal <tanakamaruko1121n@gmail.com>
2024-04-28 13:34:32 +0900

    ディレクトリ構成変更による修正

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import UserInfo from "./screens/UserInfo";
-import Home from "./screens/Home";
+import UserInfo from "./components/pages/UserInfo";
+import Home from "./components/pages/Home";
 
 const App: React.FC = () => {
   return (
---
+++ client/src/components/pages/UserInfo.tsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import { createUseStyles } from "react-jss";
+
+type User = {
+  age: number;
+  name: string;
+};
+
+const UserInfo: React.FC = () => {
+  const classes = useStyles();
+  const [userInfo, setUserInfo] = useState<User[] | null>(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("/firestore/get");
+        const json: React.SetStateAction<User[] | null> = await res.json();
+        setUserInfo(json);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <div className="container">
+    <h1>User Info.</h1>
+    <div className={classes.userBox}>
+      <div className={classes.nameTitle}>Name</div>
+      <div className={classes.ageTitle}>Age</div>
+    </div>
+    {userInfo?.map((user) => (
+        <div className={classes.userBox} key={user.name}>
+          <div className={classes.name}>{user.name}</div>
+          <div className={classes.age}>{user.age}</div>
+        </div>
+      )
+    )}
+  </div>
+  )
+};
+
+const styles = {
+  nameTitle: {
+    width: "50%",
+    padding: "10px 20px",
+    background: "#ffa500",
+    textAlign: "center",
+    border:"solid #ffa500",
+    fontWeight: "bold",
+    fontSize: "20px"
+  },
+  ageTitle: {
+    width: "10%",
+    padding: "10px 20px",
+    background: "#ffd700",
+    border:"solid #ffd700",
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: "20px"
+  },
+  userBox: {
+    display: "flex",
+    margin: "10px"
+  },
+  name: {
+    width: "50%",
+    padding: "10px 20px",
+    background: "#fdf5e6",
+    textAlign: "center",
+    border:"solid #ffdab9"
+  },
+  age: {
+    width: "10%",
+    padding: "10px 20px",
+    background: "#ffffe0",
+    border:"solid #f5deb3",
+    textAlign: "center"
+  }
+};
+const useStyles = createUseStyles(styles);
+
+export default UserInfo;
