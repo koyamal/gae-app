1dfaafaab16c8da41b100425af933261b61793e6
koyamal <tanakamaruko1121n@gmail.com>
2024-05-05 22:20:28 +0900

    UserAddページを作成した。

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Home from "./components/pages/Home";
 import UserInfo from "./components/pages/UserInfo";
 import OneUserInfo from "./components/pages/OneUserInfo";
+import UserAdd from "./components/pages/UserAdd";
 
 const App: React.FC = () => {
   return (
@@ -11,11 +12,14 @@ const App: React.FC = () => {
         <Link to="/">Home</Link>
         <br />
         <Link to="/user">UserInfo</Link>
+        <br />
+        <Link to="/adduser">UserAdd</Link>
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/user" element={<UserInfo />} />
         <Route path="/oneuserinfo/:docId" element={<OneUserInfo />} />
+        <Route path="/addUser" element={<UserAdd />} />
       </Routes>
     </BrowserRouter>
     // <UserInfo />
