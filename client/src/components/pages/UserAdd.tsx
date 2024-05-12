e89083203a610aa8070f4f966ad95f4b46dbe276
koyamal <tanakamaruko1121n@gmail.com>
2024-05-12 09:44:04 +0900

    onChangeイベントを追加

--- client/src/components/pages/UserAdd.tsx
+++ client/src/components/pages/UserAdd.tsx
@@ -7,11 +7,11 @@ import User from '../../types/User';
 const UserAdd: React.FC = () => {
   const [userName, setUserName] = useState<string>("");
   const [userAge, setUserAge] = useState<number>(NaN);
-  const [userCountry, setUserCountry] = useState<number>(NaN);
-  const [userGender, setUserGender] = useState<number>(NaN);
-  const [userJob, setUserJob] = useState<number>(NaN);
-  const [userEmail, setUserEmail] = useState<number>(NaN);
-  const [userImageUrl, setUserImageUrl] = useState<number>(NaN);
+  const [userCountry, setUserCountry] = useState<string>("");
+  const [userGender, setUserGender] = useState<string>("");
+  const [userJob, setUserJob] = useState<string>("");
+  const [userEmail, setUserEmail] = useState<string>("");
+  const [userImageUrl, setUserImageUrl] = useState<string>("");
   const [isDetail, setIsDetail] = useState<boolean>(false);
   const [msgInfo, setMsgInfo] = useState<string>("");
 
@@ -78,16 +78,40 @@ const UserAdd: React.FC = () => {
         {isDetail && (
           <div>
             <div>
-              country: <input type="text" />
+              country: 
+              <input 
+                type="text"
+                onChange={(event) => {
+                  setUserCountry(event.target.value);
+                }}
+              />
             </div>
             <div>
-              job: <input type="text" />
+              job: 
+              <input
+                type="text"
+                onChange={(event) => {
+                  setUserJob(event.target.value);
+                }}
+              />
             </div>
             <div>
-              gender: <input type="text" />
+              gender:
+              <input
+                type="text"
+                onChange={(event) => {
+                  setUserGender(event.target.value);
+                }}
+              />
             </div>
             <div>
-              email: <input type="text" />
+              email:
+              <input
+                type="text"
+                onChange={(event) => {
+                  setUserEmail(event.target.value);
+                }}
+              />
             </div>
           </div>
         )}
