5a5022328c958e74a7a153329f8edccc132c4d0c
koyamal <tanakamaruko1121n@gmail.com>
2024-05-31 21:04:57 +0900

    コメントアウト

--- client/src/components/molecules/ImageUploader.tsx
+++ client/src/components/molecules/ImageUploader.tsx
@@ -26,7 +26,7 @@ const ImageUploader: React.FC<Props> = (props) => {
   return (
     <div className="App">
       <ImageUploading
-        multiple
+        // multiple
         value={images}
         onChange={onChange}
         maxNumber={maxNumber}
@@ -50,12 +50,12 @@ const ImageUploader: React.FC<Props> = (props) => {
               Click or Drop here
             </button>
             &nbsp;
-            <button onClick={onImageRemoveAll}>Remove all images</button>
+            {/* <button onClick={onImageRemoveAll}>Remove all images</button> */}
             {imageList.map((image, index) => (
               <div key={index} className="image-item">
                 <img src={image.dataURL} alt="" width="100" />
                 <div className="image-item__btn-wrapper">
-                  <button onClick={() => onImageUpdate(index)}>Update</button>
+                  {/* <button onClick={() => onImageUpdate(index)}>Update</button> */}
                   <button onClick={() => onImageRemove(index)}>Remove</button>
                 </div>
               </div>
