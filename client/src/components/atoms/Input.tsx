80716e1045f30ef2edef0ec44e6981be38df62c6
koyamal <tanakamaruko1121n@gmail.com>
2024-06-23 17:26:09 +0900

    デザインの微修正とplaceholderの追加

--- client/src/components/atoms/Input.tsx
+++ client/src/components/atoms/Input.tsx
@@ -4,14 +4,17 @@ import { createUseStyles } from "react-jss";
 interface Props {
   onChangeEvent:(event: any) => void;
   type: string;
+  placeholder: string;
 }
 
 const Input: React.FC<Props> = (props) => {
-  const {onChangeEvent, type} = props;
+  const {onChangeEvent, type, placeholder} = props;
   const classes = useStyles();
   return (
     <input
+      className={classes.inputBody}
       type={type}
+      placeholder={placeholder}
       onChange={(event) => {onChangeEvent(event.target.value)}}
     />
   );
@@ -20,6 +23,12 @@ const Input: React.FC<Props> = (props) => {
 const styles = {
   inputContainer: {
   },
+  inputBody: {
+    height: "35px",
+    width: "300px",
+    fontSize: "20px",
+    padding: "5px"
+  }
 };
 const useStyles = createUseStyles(styles);
 
