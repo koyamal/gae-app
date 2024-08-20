a492318b3b55852f484f76479541abf523c47f11
koyamal <tanakamaruko1121n@gmail.com>
2024-08-20 20:27:12 +0900

    修正

--- server/src/server.ts
+++ server/src/server.ts
@@ -168,4 +168,4 @@ const server = app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
 server.keepAliveTimeout = 700 * 1000;
-server.headersTimeout = 701 * 1000;
+// server.headersTimeout = 701 * 1000;
