b521f8f6cfbb3b9a37d29a6fb855d8e9afce1914
koyamal <74910401+koyamal@users.noreply.github.com>
2025-03-08 09:02:07 +0900

    UseUseRefをHomeで使用

--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { createUseStyles } from "react-jss";
 
+import UseUseRef from '../molecules/useUseRef';
+
 const Home: React.FC = () => {
   return (
     <div>
         <h1>Home</h1>
+        <UseUseRef></UseUseRef>
     </div>
   );
 };
