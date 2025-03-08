50b3b2cfd9c7947bdabfba76b6d1288afde8c63d
koyamal <74910401+koyamal@users.noreply.github.com>
2025-03-08 09:05:37 +0900

    stateを更新することでレンダリングできるようにした

--- client/src/components/molecules/useUseRef.tsx
+++ client/src/components/molecules/useUseRef.tsx
@@ -1,16 +1,23 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 function UseUseRef() {
   const countRef = useRef(0);
+  const [countState, setCountState] = useState<number>(0);
 
   const incrementRef = () => {
     countRef.current += 1;
   }
 
+  const incrementState = () => {
+    setCountState(countState + 1);
+  }
+
   return (
     <div>
-      <p>{countRef.current}</p>
+      <p>countRef: {countRef.current}</p>
       <button onClick={incrementRef}>incrementRef</button>
+      <p>レンダリング: {countState}</p>
+      <button onClick={incrementState}>レンダリング</button>
     </div>
   )
 }
