59cc93717e6ad404209658ef5437d2fd145cf1fa
koyamal <74910401+koyamal@users.noreply.github.com>
2025-03-13 18:49:09 +0900

    BarContextを利用

--- client/src/components/context/useBarContext.tsx
+++ client/src/components/context/useBarContext.tsx
@@ -4,14 +4,13 @@ import { BarContext } from "./bar";
 const UseBarContext: React.FC = () => {
   let bar = useContext(BarContext);
 
-  useEffect(() => {
-    bar = 'hello'
-  }, []);
-
   return (
+    <>
     <div>
-      {bar ? <div>{bar}</div>: <div>null</div>}
+      {bar.bar ? <div>{bar.bar}</div>: <div>null</div>}
     </div>
+    <button onClick={() => {bar.changeBar('hello')}}>hello</button>
+    </>
   );
 }
 
