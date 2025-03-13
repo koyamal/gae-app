aabdf3c5e120773c03f6eb993ef9eb6eb38c360f
koyamal <74910401+koyamal@users.noreply.github.com>
2025-03-13 18:36:56 +0900

    Contextを利用

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,20 +5,23 @@ import UserInfo from "./components/pages/UserInfo";
 import OneUserInfo from "./components/pages/OneUserInfo";
 import UserAdd from "./components/pages/UserAdd";
 import MyDrawer from "./components/molecules/MyDrawer";
+import { BarContext } from "./components/context/bar";
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <MyDrawer />
-      </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/user" element={<UserInfo />} />
-        <Route path="/oneuserinfo/:docId" element={<OneUserInfo />} />
-        <Route path="/addUser" element={<UserAdd />} />
-      </Routes>
-    </BrowserRouter>
+    <BarContext.Provider value={null}>
+      <BrowserRouter>
+        <div className="App">
+          <MyDrawer />
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/user" element={<UserInfo />} />
+          <Route path="/oneuserinfo/:docId" element={<OneUserInfo />} />
+          <Route path="/addUser" element={<UserAdd />} />
+        </Routes>
+      </BrowserRouter>
+    </BarContext.Provider>
     // <UserInfo />
   );
 };
---
+++ client/src/components/context/useBarContext.tsx
@@ -0,0 +1,14 @@
+import React, {useContext} from "react";
+import { BarContext } from "./bar";
+
+const UseBarContext: React.FC = () => {
+  const bar = useContext(BarContext);
+
+  return (
+    <div>
+      {bar && <div>{bar}</div>}
+    </div>
+  );
+}
+
+export default UseBarContext;
