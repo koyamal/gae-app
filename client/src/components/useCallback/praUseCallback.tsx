f8ce43d053ee6100d7d43c4456efe2889241599a
koyamal <74910401+koyamal@users.noreply.github.com>
2025-03-16 18:30:25 +0900

    useCallbackを復習

---
+++ client/src/components/useCallback/praUseCallback.tsx
@@ -0,0 +1,17 @@
+import { useCallback, useRef } from "react";
+
+const PraUseCallback = () => {
+  const fn1 = () => {
+    console.log('fn1');
+  }
+
+  const fn2 = useCallback(() => {
+    console.log('fn2');
+  }, []);
+
+  return (
+    <div>hello</div>
+  );
+}
+
+export default PraUseCallback;
