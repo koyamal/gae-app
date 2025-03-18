81975603a3b4b59eab37094f09de0273ad70e2bf
koyamal <74910401+koyamal@users.noreply.github.com>
2025-03-18 21:09:50 +0900

    HomeでPraUseCallbackを利用

--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { createUseStyles } from "react-jss";
 
 import UseUseRef from '../molecules/useUseRef';
+import PraUseCallback from '../useCallback/praUseCallback';
 
 const Home: React.FC = () => {
   return (
     <div>
         <h1>Home</h1>
+        <PraUseCallback></PraUseCallback>
         <UseUseRef></UseUseRef>
     </div>
   );
