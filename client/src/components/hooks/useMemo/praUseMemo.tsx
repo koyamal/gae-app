60db72137426448781c27f901df528946dd37c83
koyamal <74910401+koyamal@users.noreply.github.com>
2025-03-20 09:35:41 +0900

    Child1,2のpropsをstateに変更

--- client/src/components/hooks/useMemo/praUseMemo.tsx
+++ client/src/components/hooks/useMemo/praUseMemo.tsx
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
 
-const Child1 = (props: {val1: string}) => {
+const Child1 = (props: {val1: boolean}) => {
   console.log("Child1がレンダリングされた！");
   return <p>Value: {props.val1}</p>;
 };
 
-const Child2 = React.memo((props:{val1: string}) => {
+const Child2 = React.memo((props:{val1: boolean}) => {
   console.log("Child2がレンダリングされた！");
   return <p>Count: {props.val1}</p>;
 });
 
 const Parent = () => {
   const [parentState, setParentState] = useState(false);
+  const [c1State, setC1State] = useState(false);
+  const [c2State, setC2State] = useState(false);
   return (
     <>
       <button onClick={() => setParentState(!parentState)}>parentState</button>
-      <Child1 val1={'hello'}></Child1>
-      <Child2 val1={'bye'}></Child2>
+      <button onClick={() => setC1State(!c1State)}>child01</button>
+      <button onClick={() => setC2State(!c2State)}>child02</button>
+      <Child1 val1={c1State}></Child1>
+      <Child2 val1={c2State}></Child2>
     </>
   );
 }
