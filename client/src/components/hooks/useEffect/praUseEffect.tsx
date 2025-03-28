df714bb9a7b275c6d86e06cb9116764d020da37d
koyamal <74910401+koyamal@users.noreply.github.com>
2025-03-28 23:21:24 +0900

    修正

--- client/src/components/hooks/useEffect/praUseEffect.tsx
+++ client/src/components/hooks/useEffect/praUseEffect.tsx
@@ -14,6 +14,9 @@ const Child1 = (props: {state: boolean}) => {
 
   useEffect(() => {
     console.log('start: with state');
+    if(useRefValue) {
+      console.log('hello');
+    }
     if(props.state) {
       console.log('true');
     }
