2a8a5e8c72be8c03bdadf2a585e9aeae1fb4afb3
koyamal <74910401+koyamal@users.noreply.github.com>
2025-04-03 22:26:44 +0900

    修正

--- client/src/components/molecules/child01.tsx
+++ client/src/components/molecules/child01.tsx
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 interface Props {
   func: () => void
 }
 
 const Child01 = function(props: Props) {
+  const [state1, setState1] = useState(true);
   useEffect(() => {
     const { func } = props;
     func();
