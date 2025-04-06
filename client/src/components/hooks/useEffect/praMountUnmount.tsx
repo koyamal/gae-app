156937cf81169ea08470d9845e10b17562bedd3f
koyamal <74910401+koyamal@users.noreply.github.com>
2025-04-06 10:29:35 +0900

    マウントされたタイミングでログを出力

--- client/src/components/hooks/useEffect/praMountUnmount.tsx
+++ client/src/components/hooks/useEffect/praMountUnmount.tsx
@@ -1,4 +1,9 @@
+import { useEffect } from "react";
+
 const Comp1 = function() {
+  useEffect(() => {
+    console.log('Comp1がマウントされました。');
+  }, []);
   return (
     <>
       <div>This is Comp1</div>
