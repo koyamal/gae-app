b72144d199a75481d5937e5ac31ae22e7bc47867
koyamal <74910401+koyamal@users.noreply.github.com>
2025-04-09 20:55:06 +0900

    stateを追加

--- client/src/components/hooks/useEffect/praMountUnmount.tsx
+++ client/src/components/hooks/useEffect/praMountUnmount.tsx
@@ -1,6 +1,7 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Comp1 = function() {
+  const [state1, setState1] = useState(0);
   useEffect(() => {
     console.log('Comp1がマウントされました。');
 
